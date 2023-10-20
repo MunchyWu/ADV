286e85d627cdaaba65678bd90481ed9da37b61f9
MunchyWu <132419851+MunchyWu@users.noreply.github.com>
2023-10-20 11:04:20 -0700

    Update main.py

--- Project2/main.py
+++ Project2/main.py
@@ -4,7 +4,7 @@ from js import document
 data = None
 
 def setup():
-  p5.createCanvas(300, 300)
+  p5.createCanvas(500, 500)
   print('hello p5!')
 
 def draw():
@@ -16,7 +16,19 @@ def draw():
   circle_size = int(data)
   p5.noStroke()
   p5.fill(150)
-  p5.ellipse(150, 150, circle_size, circle_size)
+  #p5.ellipse(150, 150, circle_size, circle_size)
+  p5.push()
+  angle = int(data)
+  #move to middle of canvas
+  p5.translate(p5.width/2,p5.height/2)
+  # rotate canvas with angle converted from degrees to radians:
+  p5.rotate(p5.radians(angle))
+  # change mode to daw rectangles from center:
+  p5.rectMode(pt.CENTER)
+  # draw rectangle at coordinate 0, 0 and 100 width and height:
+  p5.rect(0,0,100,100)
+  #rectore graphical transformations:
+  p5.pop()
 
 def print_test(x):
   print(x)
